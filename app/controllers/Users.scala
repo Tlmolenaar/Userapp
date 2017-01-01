3e0ea91fa03e86ea5cbd3c3ace67b48de55e10a0
Thomas Molenaar <tlmolenaar@hotmail.com>
2017-01-01 20:00:10 +0000

    created routes and Users controller

---
+++ app/controllers/Users.scala
@@ -0,0 +1,15 @@
+package controllers
+
+import javax.inject.{Inject, Singleton}
+
+import play.api.mvc.Controller
+
+@Singleton
+class Users @Inject extends Controller {
+
+  def index = TODO
+
+  def add = TODO
+
+  def save = TODO
+}
