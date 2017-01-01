8304535526b4b872718494feef6411594e373572
Thomas Molenaar <tlmolenaar@hotmail.com>
2017-01-01 20:53:40 +0000

    Added logger and MDC

--- app/controllers/Users.scala
+++ app/controllers/Users.scala
@@ -4,6 +4,7 @@ import models.User
 import play.api._
 import javax.inject.{Inject, Singleton}
 
+
 import play.api.data._
 import play.api.data.Forms._
 import play.api.mvc.{Action, Controller}
@@ -18,12 +19,20 @@ import reactivemongo.api.Cursor
 import reactivemongo.play.json._
 import reactivemongo.play.json.collection.JSONCollection
 
+import org.slf4j.MDC
+import play.api.Logger
+
 @Singleton
 class Users @Inject()(val messagesApi: MessagesApi, val reactiveMongoApi: ReactiveMongoApi)  extends Controller with I18nSupport with MongoController with ReactiveMongoComponents {
 
 
 
   def collection: JSONCollection = db.collection[JSONCollection]("user")
+
+  //The logger has been extended so it will provide both a timestamp and the thread
+  // MDC adds information about the method to the logger
+
+  val logger: Logger = Logger(this.getClass())
 
   //Created a simple form.
 
@@ -34,8 +43,12 @@ class Users @Inject()(val messagesApi: MessagesApi, val reactiveMongoApi: Reacti
 
   val userForm: Form[User] = Form(formMapping)
 
+  //The index page automatically lists all the contacts in the database
+
   def index = Action.async { implicit request =>
 
+    MDC.put("method", "all")
+    logger.info("requested to retrieve all people")
 
     val cursor: Cursor[User] = collection.find(Json.obj()).cursor[User]
 
@@ -49,26 +62,42 @@ class Users @Inject()(val messagesApi: MessagesApi, val reactiveMongoApi: Reacti
     }
   }
 
-
+  //Add leads to the view with the simple form
 
   def add = Action {
 
-        Ok(views.html.user.form(userForm))
+    logger.info("requested to add user")
+
+    Ok(views.html.user.form(userForm))
 
   }
 
+  /* When a new request is sent to the database the bindFromRequest method offers two options.
+    If the request is invalid (if the entry can't be validated) the form is shown again together
+    with standard error messages.
+
+    If the request is processed correctly the controller will redirect to the index page where
+    the new entry is shown in a list.
+  */
   def save = Action.async {
     implicit request =>
+
+      MDC.put("method", "save")
+      logger.info("requested to save user")
 
       userForm.bindFromRequest.fold(
         errors => {
 
+          logger.error(s"${errors.toString()} while trying to add user")
+
           Future.successful(
-            BadRequest(views.html.user.form(errors))
-          )
+            BadRequest(views.html.user.form(errors)))
         },
 
         user => {
+
+          logger.debug(s"user to add: ${user.toString}")
+          logger.info("successfully added user")
 
           val futureResult = collection.insert(user)
 
